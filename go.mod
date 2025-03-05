47c1a23ee12cdc85d47464d714e94dc40b895d61
John Verity <john@flowrolled.com>
2025-03-05 17:09:49 -0500

    limits rate

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.24
 require (
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/go-playground/validator/v10 v10.25.0
+	github.com/spf13/viper v1.19.0
+	golang.org/x/time v0.5.0
 )
 
 require (
@@ -23,7 +25,6 @@ require (
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.19.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"golang.org/x/time/rate"
 
 	"github.com/realjv3/event-agg/interfaces/rest"
 	"github.com/realjv3/event-agg/services/events"
@@ -12,6 +15,12 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/spf13/viper"
 )
+
+var limiter *rate.Limiter
+
+func init() {
+	limiter = rate.NewLimiter(10, 20)
+}
 
 func main() {
 	log.Println("Initializing config...")
@@ -35,8 +44,20 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(time.Second * 20))
+	r.Use(rateLimiter)
 
 	rest.NewEventHandler(r, queue)
 
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func rateLimiter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := limiter.Wait(r.Context())
+		if err != nil {
+			fmt.Println("Rate limit exceeded")
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
